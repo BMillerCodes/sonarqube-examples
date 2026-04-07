0a6e097bb7f754800eefb7d562f6ce93efc2dbcb
BMillerCodes <cypressbam@gmail.com>
2026-04-07 14:24:17 -0500

    Remove objc, php, plsql, and shell example files and workflows from feat/sonarqube-mvps

--- objc/src/Helper.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface Helper : NSObject
-+ (int)multiply:(int)a b:(int)b;
-+ (BOOL)isValid:(int)value;
-+ (NSString *)formatMessage:(NSString *)message;
-@end
